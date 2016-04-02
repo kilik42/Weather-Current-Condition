58e41c11ea7b913074ea4022d0ca36a6023e5463
marvin evins <maevins@cps.edu>
2016-04-01 21:38:41 -0500

    weatherApp
    sample weather app for chicago

---
+++ Weather Current Condition/ViewController.swift
@@ -0,0 +1,88 @@
+//
+//  ViewController.swift
+//  Weather Current Condition
+//
+//  Created by marvin evins on 3/21/16.
+//  Copyright © 2016 marvin evins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var cityTextField: UITextField!
+    
+    @IBOutlet var resultLabel: UILabel!
+    
+    @IBAction func findWeather(sender: AnyObject) {
+        
+        var wasSuccessful = false
+        let attemptedUrl = NSURL(string: "http://www.weather-forecast.com/locations/" + cityTextField.text!.stringByReplacingOccurrencesOfString(" ", withString: "-") + "/forecasts/latest")
+        
+        if let url = attemptedUrl {
+            
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url) {
+            (data, response, error) -> Void in
+            
+            if let urlContent = data {
+                let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
+                
+                let websiteArray = webContent?.componentsSeparatedByString("3 Day Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">")
+                
+                if websiteArray!.count > 1{
+                    
+                    
+                    
+                    let weatherArray = websiteArray![1].componentsSeparatedByString("</span>")
+                    
+                    //print(weatherArray[0])
+                    
+                    if weatherArray.count > 1{
+                        let weatherSummary = weatherArray[0].stringByReplacingOccurrencesOfString("&deg", withString: "º")
+                        
+                        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                            
+                            self.resultLabel.text = weatherSummary
+                            
+                        })
+                        
+                    }
+                    
+                    
+                }
+                
+                
+            }
+            
+            if wasSuccessful == false{
+                
+                self.resultLabel.text = "couldn't find the weather for that city - please try again"
+           }
+            
+            
+         }
+        
+        task.resume()
+        } else {
+            self.resultLabel.text = "couldn't find the weather for that city - please try again"
+        }
+        
+    }
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+            }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
